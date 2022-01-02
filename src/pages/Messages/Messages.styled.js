610d8c9c4b0b53f703af7380e394f6b0819ff1a0
zafar <zafar@mudah.my>
2022-01-02 19:21:07 +0500

    feat: Compose/Home page is added

---
+++ src/pages/Messages/Messages.styled.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+	responsive: {
+		display: 'block',
+		maxWidth: '100%',
+		margin: '0 auto',
+	},
+	normal: {
+		
+	}
+});
+
